cf4e40c3be586581ff2a85f4618b4725dfcb29e9
Feras Shaer <Ferasmo7mad@gmail.com>
2020-02-16 20:04:20 +0200

    work on historiable traits

---
+++ src/Traits/History/ColumnChanges.php
@@ -0,0 +1,27 @@
+<?php
+/**************************************************************************
+ * Copyright (C) iFeras93, Inc - All Rights Reserved
+ *
+ *
+ * @file        ColumnChanges.php
+ * @author      Firas
+ * @project     HistoriableModel
+ * @date        2/16/2020
+ */
+
+namespace Iferas93\HistoriableModel\Traits\History;
+
+
+class ColumnChanges
+{
+    public $column;
+    public $from;
+    public $to;
+
+    public function __construct($col, $from_value, $to_value)
+    {
+        $this->column = $col;
+        $this->from = $from_value;
+        $this->to = $to_value;
+    }
+}
--- src/Traits/History/Histroiable.php
+++ src/Traits/History/Histroiable.php
@@ -13,16 +13,47 @@ namespace Iferas93\HistoriableModel\Traits\History;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 
 trait Histroiable
 {
     public function bootHistroiable()
     {
+        static::updated(function (Model $model) {
+            $columns = $this->getChangedColumns($model);
+            collect($columns)->each(function ($column) use ($model) {
+                $model->saveChanges($column);
+            });
+        });
+    }
 
+    private function getChangedColumns(Model $model)
+    {
+        $original = $model->getOriginal();
+        return collect(Arr::except($model->getChanges(), $this->ignoredColumns()))->map(function ($change, $column) use ($original) {
+            return new ColumnChanges($column, Arr::get($original, $column), $change);
+        });
+    }
+
+    protected function ignoredColumns()
+    {
+        return [
+            'updated_at'
+        ];
+    }
+
+    private function saveChanges(ColumnChanges $column)
+    {
+        $this->history()->create([
+            "changed_column" => $column->column,
+            "value_from" => $column->from,
+            "value_to" => $column->to,
+        ]);
     }
 
     public function history()
     {
-        return $this->morphMany(config('historiable.model'),"Historiable");
+        return $this->morphMany(config('historiable.model'), "Historiable");
     }
 }
