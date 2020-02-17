53bd18c5719862ca16d4ba4bc4ebba233248494c
Feras Shaer <Ferasmo7mad@gmail.com>
2020-02-17 08:42:18 +0200

    Apply fixes from StyleCI (#1)

--- src/HistoriableModelServiceProvider.php
+++ src/HistoriableModelServiceProvider.php
@@ -14,26 +14,22 @@ class HistoriableModelServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../config/historiable.php' => config_path('historiable.php'),
+                __DIR__.'/../config/historiable.php' => config_path('historiable.php'),
             ], 'config');
 
-            if (!class_exists("CreateHistoriesTable")) {
+            if (! class_exists('CreateHistoriesTable')) {
                 $this->publishes([
-                    __DIR__ . '/../src/migrations/create_histories_table.php.stub.php'
-                    => database_path('migrations/' . date("Y_m_d_His", time()) . "_create_histories_table.php")
+                    __DIR__.'/../src/migrations/create_histories_table.php.stub.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_histories_table.php'),
                 ], 'migrations');
-
             }
 
-
-           /* if (!class_exists("History")) {
-                // Publishing the models.
-                $this->publishes([
-                    __DIR__ . '/../src/Models/History.php' => app_path('Models/History.php'),
-                ], 'models');
-            }*/
+            /* if (!class_exists("History")) {
+                 // Publishing the models.
+                 $this->publishes([
+                     __DIR__ . '/../src/Models/History.php' => app_path('Models/History.php'),
+                 ], 'models');
+             }*/
         }
-
     }
 
     /**
@@ -42,7 +38,7 @@ class HistoriableModelServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__ . '/../config/historiable.php', 'historiable');
+        $this->mergeConfigFrom(__DIR__.'/../config/historiable.php', 'historiable');
 
         // Register the main class to use with the facade
         $this->app->singleton('historiable', function () {
