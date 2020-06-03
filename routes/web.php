db4c490019f86027240c5f37662d6cc654bae977
Feras Shaer <Ferasmo7mad@gmail.com>
2020-06-03 19:43:39 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use \Illuminate\Support\Facades\Route;
-use \Iferas93\HistoriableModel\Http\Controllers\HistoryController;
+use Iferas93\HistoriableModel\Http\Controllers\HistoryController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/changelog', [HistoryController::class, 'index'])->name('changelog.index');
 Route::get('/changelog/{id}', [HistoryController::class, 'show'])->name('changelog.show');
--- src/Http/Controllers/HistoryController.php
+++ src/Http/Controllers/HistoryController.php
@@ -2,26 +2,24 @@
 
 namespace Iferas93\HistoriableModel\Http\Controllers;
 
-
 use Iferas93\HistoriableModel\Models\History;
-use Illuminate\Http\Request;
 
 class HistoryController extends Controller
 {
-
     /**
-     * Show History of changes column
+     * Show History of changes column.
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
         $changelog = History::query()->orderByDesc('id')->paginate();
+
         return view('historiable::index', compact(['changelog']));
     }
 
     /**
-     * Show specific item by ID
+     * Show specific item by ID.
      * @param History $history
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
@@ -29,6 +27,7 @@ class HistoryController extends Controller
     {
         //return $id;
         $changelogById = History::query()->where('id', $id)->first();
+
         return view('historiable::show', compact(['changelogById']));
     }
 }
