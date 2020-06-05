378e78520703ef05d0cc3f8ea9345ea0eaefd9c0
Feras Mohamed <ferasmo7mad@gmail.com>
2020-06-05 23:12:22 +0300

    create command to install (migration,config file)

---
+++ src/Console/Commands/InstallPackage.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallPackage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'historiable:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'publish package assets';
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Installing Historiable...');
+        $this->info('Publishing configuration...');
+        $this->call('vendor:publish', [
+            '--provider' => "Iferas93\HistoriableModel\HistoriableModelServiceProvider",
+            '--tag' => "config"
+        ]);
+
+        $this->info('Publishing migrations...');
+        $this->call('vendor:publish', [
+            '--provider' => "Iferas93\HistoriableModel\HistoriableModelServiceProvider",
+            '--tag' => "migrations"
+        ]);
+
+        $this->info('Historiable has been Installed successfully');
+    }
+}
--- src/HistoriableModelServiceProvider.php
+++ src/HistoriableModelServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Iferas93\HistoriableModel;
 
+use App\Console\Commands\InstallPackage;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,16 +24,20 @@ class HistoriableModelServiceProvider extends ServiceProvider
                 ], 'migrations');
             }
 
-            /* if (!class_exists("History")) {
+             /*if (!class_exists("History")) {
                  // Publishing the models.
                  $this->publishes([
                      __DIR__ . '/../src/Models/History.php' => app_path('Models/History.php'),
                  ], 'models');
              }*/
+
+            $this->commands([
+                InstallPackage::class,
+            ]);
         }
 
-        $this->loadRoutesFrom(__DIR__.'../../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'historiable');
+        $this->loadRoutesFrom(__DIR__ . '../../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'historiable');
         Paginator::useTailwind();
     }
 
@@ -42,7 +47,7 @@ class HistoriableModelServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/historiable.php', 'historiable');
+        $this->mergeConfigFrom(__DIR__ . '/../config/historiable.php', 'historiable');
 
         // Register the main class to use with the facade
         $this->app->bind('historiable', function () {
